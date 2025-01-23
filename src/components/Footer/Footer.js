39098f4095e3c5a2c30de186a919bc43907abd6e
DESKTOP-Q4B6B51\JITHIN KM <jithinmuralikm@gmail.com>
2025-01-23 17:02:19 +0530

    new changes

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,20 +7,20 @@ const Footer = () => {
         <footer className="footer">
             <div className="footer-container">
                 {/* Hidden WhatsApp Icon on the left */}
-                <div className="footer-socials hidden-icon">
+                {/* <div className="footer-socials hidden-icon">
                     <a href="https://wa.me/7994396662" target="_blank" rel="noreferrer">
                         <FaWhatsapp size={48} className="big-whatsapp-icon" />
                     </a>
-                </div>
+                </div> */}
                 <div className="footer-logo">
                     <h3>KAPRA WEB AI MAKERS PVT LTD</h3>
                     <p className="subsidiary-text">Subsidiary of Kapra Highness Ventures Pvt Ltd</p>
                 </div>
-                <div className="footer-socials hidden-icon">
+                {/* <div className="footer-socials hidden-icon">
                     <a href="https://wa.me/7994396662" target="_blank" rel="noreferrer">
                         <FaWhatsapp size={48} className="big-whatsapp-icon" />
                     </a>
-                </div>
+                </div> */}
             </div>
             <p>© 2024 KAPRA WEB AI MAKERS PVT LTD. All rights reserved.</p>
         </footer>
